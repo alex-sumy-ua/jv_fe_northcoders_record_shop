6bbc1780082b7778276942cc6f486e439c99aaea
alex-sumy-ua <plachkovskyy@gmail.com>
2024-09-19 19:40:08 +0100

    feat: Task 5: Update ViewModel - completed

--- app/src/main/java/com/northcoders/jv_fe_northcoders_record_shop/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/jv_fe_northcoders_record_shop/ui/mainactivity/MainActivityViewModel.java
@@ -1,15 +1,17 @@
 package com.northcoders.jv_fe_northcoders_record_shop.ui.mainactivity;
 
 import android.app.Application;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-
 import com.northcoders.jv_fe_northcoders_record_shop.model.Album;
 import com.northcoders.jv_fe_northcoders_record_shop.model.AlbumRepository;
-
 import java.util.List;
+
+
+//import androidx.lifecycle.MutableLiveData;
+//import com.northcoders.jv_fe_northcoders_record_shop.service.AlbumApiService;
+//import com.northcoders.jv_fe_northcoders_record_shop.service.RetrofitInstance;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
@@ -25,4 +27,13 @@ public class MainActivityViewModel extends AndroidViewModel {
     public LiveData<List<Album>> getAllAlbums() {
         return allAlbums;
     }
+
+//    public LiveData<String> getToastMessage() {
+//        return toastMessage;
+//    }
+
+    public void addAlbum(Album album) {
+        albumRepository.addAlbum(album);
+    }
+
 }
